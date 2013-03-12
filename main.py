1f5f9facf5674c169dd2ce6437114a33405d8dbb
Ong Jun Hao Bryan <ong.junhao.bryan@dhs.sg>
2013-03-12 13:58:51 +0800

    added parent child hierarchies

--- main.py
+++ main.py
@@ -17,24 +17,26 @@ class Food(db.Model):
   picture = db.BlobProperty()
   price = db.FloatProperty()
   rating = db.IntegerProperty()
-  
+
+
 class Review(db.Model):
   author = db.StringProperty()
   content = db.StringProperty(multiline=True)
   date = db.DateTimeProperty(auto_now_add=True)
   rating = db.IntegerProperty()
+  reference = db.ReferenceProperty(Food)
 
 
-def guestbook_key(guestbook_name=None):
-  """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
-  return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
+def stall_key(stall_name=None):
+  """Constructs a Datastore key for a stall entity with stall_name."""
+  return db.Key.from_path('Stall', stall_name or 'default_stall')
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        guestbook_name=self.request.get('guestbook_name')
+        stall_name=self.request.get('stall_name')
         reviews_query = Review.all().ancestor(
-            guestbook_key(guestbook_name)).order('-date')
+            stall_key(stall_name)).order('-date')
         reviews = reviews_query.run(limit=10)
 
         if users.get_current_user():
@@ -45,6 +47,7 @@ class MainPage(webapp2.RequestHandler):
             url_linktext = 'Login'
 
         template_values = {
+        template_values = {
             'reviews': reviews,
             'url': url,
             'url_linktext': url_linktext,
@@ -53,21 +56,24 @@ class MainPage(webapp2.RequestHandler):
         template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render(template_values))
 
-class Guestbook(webapp2.RequestHandler):
+
+class Stall(webapp2.RequestHandler):
   def post(self):
     # We set the same parent key on the 'review' to ensure each review is in
     # the same entity group. Queries across the single entity group will be
     # consistent. However, the write rate to a single entity group should
     # be limited to ~1/second.
-    guestbook_name = self.request.get('guestbook_name')
-    review = Review(parent=guestbook_key(guestbook_name))
+    stall_name = self.request.get('stall_name')
+    review = Review(parent=stall_key(stall_name))
 
     if users.get_current_user():
       review.author = users.get_current_user().nickname()
     review.content = self.request.get('content')
     review.put()
-    self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
+    self.redirect('/?' + urllib.urlencode({'stall_name': stall_name}))
+  
+	
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/sign', Guestbook)],
+                               ('/review', Stall)],
                               debug=True)
